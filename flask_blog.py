8972136dd722f46eddcff1fc7aa4577828d8e547
ohendarko <ohendarko@gmail.com>
2024-04-06 21:20:46 +0000

    pushing saved

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '027f5ce927ba58e1709c9942adb5f5a9'
 
 posts = [
