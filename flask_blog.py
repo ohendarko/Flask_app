ae65848b45bb460507a8152308978aad1ebc19a7
ohendarko <ohendarko@gmail.com>
2024-04-07 15:10:32 +0000

    adding form info validations

--- flask_blog.py
+++ flask_blog.py
@@ -42,6 +42,7 @@ def register():
 @app.route("/login")
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
     return render_template('login.html', title='Register', form=form)
 
 
