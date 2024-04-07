2922c03f0fdbd13593ea3eb3ad2f62a109f5f86e
ohendarko <ohendarko@gmail.com>
2024-04-07 21:23:19 +0000

    building database: correcting typo

--- flask_blog.py
+++ flask_blog.py
@@ -24,7 +24,7 @@ class User(db.Model):
     class Post(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         title = db.Column(db.String(100), nullable=False)
-        date_posted = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
+        date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
         content = db.Column(db.Text, nullable=False)
         user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
