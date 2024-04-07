9f867383e6ebcfc0b3ec42edee1939eee87f9584
ohendarko <ohendarko@gmail.com>
2024-04-07 14:11:57 +0000

    adding redirect

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '027f5ce927ba58e1709c9942adb5f5a9'
@@ -29,14 +29,17 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login")
-def register():
+def login():
     form = LoginForm()
     return render_template('login.html', title='Register', form=form)
 
