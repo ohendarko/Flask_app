1cdafd91330db5495d6fdd67903664229297431a
ohendarko <ohendarko@gmail.com>
2024-04-08 19:46:25 +0000

    Structuring our flask app as a package part4: separating routes

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-import email_validator
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '027f5ce927ba58e1709c9942adb5f5a9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
+from models import User, Post
 posts = [
     {
         'author': 'KOD',
