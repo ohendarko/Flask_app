3be02b05a3994cbdf2c36362479e67ba2109d1af
ohendarko <ohendarko@gmail.com>
2024-04-17 22:12:25 +0000

    builing up user profile page with profile picture

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flask_blog.forms import RegistrationForm, LoginForm
 from flask_blog import app, db, bcrypt
 from flask_blog.models import User, Post
@@ -55,7 +55,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Register', form=form)
