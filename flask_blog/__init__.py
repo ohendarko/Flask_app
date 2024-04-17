23e1dbbd08b1b4667330c103804a3aef1ed790da
ohendarko <ohendarko@gmail.com>
2024-04-17 17:29:35 +0000

    added Mixin import. Added inheritance to user

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import  Bcrypt
+from flask_login import LoginManager
 
 import email_validator
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '027f5ce927ba58e1709c9942adb5f5a9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_blog import routes
--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -18,12 +18,12 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one')
+            raise ValidationError('This username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one')
+            raise ValidationError('This email is already in use.')
 
 
 class LoginForm(FlaskForm):
